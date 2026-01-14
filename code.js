32bda55ce294df9d511d3591ec2ef90f3aa7da4b
Mateus Alves <31938333+Alvesmateus@users.noreply.github.com>
2026-01-14 12:09:37 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,11 @@
 (function() {
-    // Tenta encontrar o elemento a cada 100 milissegundos
-    var checkExist = setInterval(function() {
-       var elemento = document.getElementById('dynamic-title');
+    // Essa função vai rodar repetidamente até encontrar o ID correto
+    var monitorarElemento = setInterval(function() {
+       var elemento = document.getElementById('titulo-dinamico-cp');
        
        if (elemento) {
           elemento.innerText = "Mateus";
-          clearInterval(checkExist); // Para de procurar assim que encontrar e mudar
-          console.log("Título alterado com sucesso!");
+          clearInterval(monitorarElemento); // Para o loop após encontrar
        }
     }, 100);
 })();
