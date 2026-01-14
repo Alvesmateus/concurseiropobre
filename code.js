38075ef297910844c087d3d085d3a38f714ed9b5
Mateus Alves <31938333+Alvesmateus@users.noreply.github.com>
2026-01-14 12:12:03 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,2 @@
-(function() {
-    const substituir = () => {
-        // Busca o elemento pelo ID ou qualquer H1 que tenha 'Carregando'
-        let el = document.getElementById('titulo-dinamico-cp') || document.querySelector('h1');
-        
-        if (el && el.innerText.includes("Carregando")) {
-            el.innerText = "Mateus";
-            console.log("H1 atualizado para Mateus!");
-            return true;
-        }
-        return false;
-    };
-
-    // Tenta executar em intervalos para garantir que o Blogger carregou o elemento
-    let tentativa = 0;
-    const interval = setInterval(() => {
-        if (substituir() || tentativa > 50) { 
-            clearInterval(interval); 
-        }
-        tentativa++;
-    }, 100);
-})();
+var el = document.getElementById('titulo-dinamico-cp');
+if(el) { el.innerText = "Mateus"; }
